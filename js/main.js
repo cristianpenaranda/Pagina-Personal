bc74806d164584e7aa87210810a2ba531454ea46
Cristian Peñaranda <37253214+cristianpenaranda@users.noreply.github.com>
2018-10-02 21:45:43 -0500

    agregado de pestañas

--- js/main.js
+++ js/main.js
@@ -3,4 +3,19 @@ window.onload = function (){
    var contenedor = document.getElementById('contenedor_carga');
    contenedor.style.visibility = 'hidden';
    contenedor.style.opacity = '0';
-}; 
+};
+
+$(document).ready(function(){
+   $('ul li a:first').addClass('active');
+   $('#contenido').load('resources/conocimientos.html');
+
+   $('.nav-link').click(function(){
+   	$('ul li a').removeClass('active');
+   	$(this).addClass('active');
+   	$('#contenido div').hide();
+
+      var link = 'resources/'+ $(this).attr('href')+'.'+'html';
+		$('#contenido').load(link);
+	    return false;
+   });
+}); 
