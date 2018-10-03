c777927f98276dd05816d9a71debf6703e008bd9
Cristian Peñaranda <37253214+cristianpenaranda@users.noreply.github.com>
2018-10-02 19:02:42 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+/*SIMBOLO DE CARGA*/
+window.onload = function (){
+   var contenedor = document.getElementById('contenedor_carga');
+   contenedor.style.visibility = 'hidden';
+   contenedor.style.opacity = '0';
+}; 
